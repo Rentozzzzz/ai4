ae0fd5f1cd8eb132ccb6679b8066dd67703f0d28
Gavin Tiling <4050481@schoolsnet.act.edu.au>
2024-06-14 11:29:14 +1000

    Challenge2

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -22,7 +22,7 @@ async def validate_password(request: PasswordRequest):
 async def download_pdf():
     pdf_file_path = 'static/DummyTest.pdf'  #Update depending on what PDF file
     return FileResponse(pdf_file_path, media_type='application/pdf', filename='DummyTest.pdf')
-
+# print(FileResponse)
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
